23a86707c5258da1c897c7db40802dab76f2bf9f
bhavani <bhavani.gdm@gmail.com>
2017-12-05 22:39:23 -0800

    bubble sort algorithm

---
+++ algorithms/bubble_sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  array
+end
+
+input = [4, 7, 2, 3, 6, 9]
+p bubble_sort(input)
+
