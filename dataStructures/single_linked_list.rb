82ce8f66eab9662fd023c0851025ac72f427605f
bhavani <bhavani.gdm@gmail.com>
2018-02-14 22:03:04 -0800

    single linked list implementation

---
+++ dataStructures/single_linked_list.rb
@@ -0,0 +1,62 @@
+class Node
+  attr_accessor :next
+  attr_reader   :value
+  def initialize(val)
+    @value = val
+    @next = nil
+  end
+end
+
+class LinkedList
+  def initialize(val = nil)
+    @head = Node.new(val)
+  end
+
+  def append(val)
+    current = @head
+    while current.next != nil
+      current = current.next
+    end
+    current.next = Node.new(val)
+  end
+
+  def append_after
+
+  end
+
+  def delete(val)
+    return if !@head
+    node=@head
+    if node.value == val
+      @head= node.next
+    end
+    if node.next
+      until node.next.value == val
+          node = node.next
+      end
+      node.next=node.next.next
+    end
+  end
+
+  def print
+    return 'linked list empty' if !@head
+    node = @head
+    nodes = []
+    while  node
+       nodes << node.value
+      node = node.next
+    end
+     nodes
+  end
+end
+
+linkedList = LinkedList.new(3)
+#linkedList.append(5)
+#linkedList.append(8)
+#linkedList.append(4)
+linkedList.delete(3)
+linkedList.delete(3)
+p linkedList.print
+
+
+
